58bada2d102ab0c76cb15ca5bf7ca625d46efcde
Hurwitzzz <75025768+Hurwitzzz@users.noreply.github.com>
2023-05-02 15:13:20 +0200

    initialize

--- Lab1/src/kernels.cpp
+++ Lab1/src/kernels.cpp
@@ -35,6 +35,15 @@ void Linear(Tensor * X, Tensor * W, Tensor * B, Tensor * Z)
 {
     uint32_t flatten_size = X->size[0] * X->size[1] * X->size[2];
     X->resize(1,1,flatten_size);
+
+    uint32_t output_size = Z->size[0] * Z->size[1] * Z->size[2];
+    for (uint32_t i=0; i<output_size; i++)
+    {
+        for (uint32_t j=0; j<flatten_size; j++)
+        {
+            Z[0][0][i] += X[0][0][j] * W[0][j][i];
+        }
+    }
 }
 
 
