8ddffffec8af55a6314c626b9459c9d3e968dfd5
Lorenz Krakau <ge93mok@mytum.de>
2023-07-10 09:54:05 +0200

    Add missing semicolons

--- final_project/conv.h
+++ final_project/conv.h
@@ -5,17 +5,17 @@
 #define OUT_CHANNEL 512
 
 // Declare the function
-float get_in(float tensor[KERNEL_SIZE*KERNEL_SIZE],int row, int column)
+float get_in(float tensor[KERNEL_SIZE*KERNEL_SIZE],int row, int column);
 
-float get_IN(float tensor[IN_CHANNEL*IN_SIZE*IN_SIZE], int ch, int row, int column, int in_w, int in_h)
+float get_IN(float tensor[IN_CHANNEL*IN_SIZE*IN_SIZE], int ch, int row, int column, int in_w, int in_h);
 
-float get_w(float tensor[KERNEL_SIZE*KERNEL_SIZE], int row, int column)
+float get_w(float tensor[KERNEL_SIZE*KERNEL_SIZE], int row, int column);
 
-float get_W(float tensor[], int z_ch, int ch, int row, int column, int in_c)
-float get_z(float tensor[OUT_SIZE * OUT_SIZE], int row, int column, int in_w)
+float get_W(float tensor[], int z_ch, int ch, int row, int column, int in_c);
+float get_z(float tensor[OUT_SIZE * OUT_SIZE], int row, int column, int in_w);
 
 
-float get_OUT(float tensor[OUT_CHANNEL*OUT_SIZE*OUT_SIZE], int ch, int row, int column, int out_w)
+float get_OUT(float tensor[OUT_CHANNEL*OUT_SIZE*OUT_SIZE], int ch, int row, int column, int out_w);
 
 
 void EntryConv(float in_sm[IN_CHANNEL*IN_SIZE*IN_SIZE],
