51b8818eae2b5b4efe59dfb562dbb2b77259ae96
Lorenz Krakau <ge93mok@mytum.de>
2023-07-10 20:24:13 +0200

    Replace get functions with manual indexing and precalculated indices

--- final_project/conv.h
+++ final_project/conv.h
@@ -4,15 +4,6 @@
 #define OUT_SIZE (IN_SIZE - KERNEL_SIZE + 1)
 #define OUT_CHANNEL 512
 
-// Declare the function
-float& get_in(float tensor[KERNEL_SIZE*KERNEL_SIZE],int row, int column);
-float& get_IN(float tensor[IN_CHANNEL*IN_SIZE*IN_SIZE], int ch, int row, int column, int in_w);
-float& get_w(float tensor[KERNEL_SIZE*KERNEL_SIZE], int row, int column);
-float& get_W(float tensor[], int z_ch, int ch, int row, int column, int in_c);
-float& get_z(float tensor[OUT_SIZE * OUT_SIZE], int row, int column, int in_w);
-float& get_OUT(float tensor[OUT_CHANNEL*OUT_SIZE*OUT_SIZE], int ch, int row, int column, int out_w);
-
-
 void EntryConv(float in_sm[IN_CHANNEL*IN_SIZE*IN_SIZE],
 	float w_sm[OUT_CHANNEL*IN_CHANNEL*KERNEL_SIZE*KERNEL_SIZE],
 	float b_sm[OUT_CHANNEL],
