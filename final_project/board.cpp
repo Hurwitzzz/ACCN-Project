37faf630bee764f6160f5370735e5e6eee2e6c9b
Lorenz Krakau <ge93mok@mytum.de>
2023-07-11 17:22:35 +0200

    Continue on board setup

--- final_project/board.cpp
+++ final_project/board.cpp
@@ -3,104 +3,102 @@
 #include <cstdlib>
 #include <cstring>
 #include <cmath>
+#include "conv.h"
+#include "common.h"
 
 extern "C"{
 #include "pynq_api.h"
-        }
-
-#define INPUT_SIZE  100
-#define KERNEL_SIZE 3
-#define OUTPUT_SIZE (INPUT_SIZE - KERNEL_SIZE + 1)
-
-void loaddat(FILE * f, int ntest,  float * x , float * w, float * ref);
-
-
-int main(){
-
-        /* connect to the leds */
-        PYNQ_loadBitstream((char * )"conv2d.bit");
-        PYNQ_MMIO_WINDOW led, hls;  // hls is AXILITE interface
-        PYNQ_createMMIOWindow(&led, 0x40010000,8); //in this functino, it uses mmap; the address can be got from vivado; 8 is length
-        PYNQ_createMMIOWindow(&hls, 0x40000000,64); //0x00 to 0x2c is 32+12=44, plus 4bytes, 48 is enough. We give 64. It will allocate the whole page anyway, so it doesn't really matter how big you set this, as long as you don't go over 4kB; This is the MMIOwindow for hls_slave, we can write to here.
-
-        /* Alocating shared memory */
-        PYNQ_SHARED_MEMORY sm_x, sm_w, sm_z;
-        PYNQ_allocatedSharedMemory(&sm_x, INPUT_SIZE * sizeof(float), 0);
-        PYNQ_allocatedSharedMemory(&sm_w, KERNEL_SIZE * sizeof(float), 0);
-        PYNQ_allocatedSharedMemory(&sm_z, OUTPUT_SIZE * sizeof(float), 0);
-
-        /* write to the address */
-        // The structure of var"led" can be got from doc
-        uint32_t * b_led = (uint32_t *) led.buffer;
-        b_led[1] = 0; // the TRI register
-        b_led[0] = 3; // DATA register
-
-
-        /* Now can write the input data to the memory address */
-        // One cannot write to the pointer to virtual memory, so cast it to float * first
-        float * virt_x = (float *) sm_x.pointer;
-        float * virt_w = (float *) sm_w.pointer;
-        float * virt_z = (float *) sm_z.pointer;
-
-        /* Copy addr over axilite */
-        memcpy(hls.buffer + 0x10, &(sm_x.physical_address), sizeof(size_t)); // 0x10 can be found in hls' synthsis summary
-        memcpy(hls.buffer + 0x1c, &(sm_w.physical_address), sizeof(size_t));
-        memcpy(hls.buffer + 0x28, &(sm_z.physical_address), sizeof(size_t));
-
-        /* Load the test data */
-        FILE * f = fopen("test.dat", "r");
-        int ntests;
-        fscanf(f,"%d",&ntests);
-    	float *z = new float[OUTPUT_SIZE * ntests];
-    	float *ref = new float[OUTPUT_SIZE * ntests];
-    	float *x = new float[INPUT_SIZE * ntests];
-    	float *w = new float[KERNEL_SIZE * ntests];
-    	loaddat(f,ntests,x,w,ref);
-
-        for(int i = 0; i < ntests; i++) {
-            memcpy(virt_x, x + i*INPUT_SIZE, sizeof(float) * INPUT_SIZE);
-            memcpy(virt_w, w + i*KERNEL_SIZE, sizeof(float) * KERNEL_SIZE);
-
-            /* Start HLS by setting bit */
-            uint32_t * hls_ctrl = (uint32_t *) hls.buffer;
-            *hls_ctrl = 0b1;
-            while((*hls_ctrl & 0b100)){}; // waiting for the IDLE(the 3rd bit) to 1, then we can print out the result
-     
-            memcpy(z + i * OUTPUT_SIZE, virt_z, sizeof(float) * OUTPUT_SIZE);
-        }
-
-		for(int n = 0; n < ntests; n++) {
-        	for (int i = 0; i < OUTPUT_SIZE; i++){
-            	if(fabs(ref[OUTPUT_SIZE*n + i] - z[OUTPUT_SIZE*n + i]) > 1e-4) {
-					printf("Error %d: z:%f != ref:%f \n",i,z[OUTPUT_SIZE*n + i],ref[OUTPUT_SIZE*n + i]);
-					return 1;
-            	}
-        	}
-		}
-
-    	printf("All values are equal!\n");
-        // don't forget to free the memory
-        delete [] ref;
-        delete [] x;
-        delete [] w;
-        delete [] z;
-        PYNQ_closeMMIOWindow( &led);
-        PYNQ_closeMMIOWindow( &hls);
-        return 0;
 }
 
+int main(){
+    auto f = openTestFile();
+    if(!f) return 1;
 
-void loaddat(FILE * f, int ntest,  float * x , float * w, float * ref)
-{
-	for(int j = 0; j < ntest ; j++){
-		for(int i = 0 ; i < INPUT_SIZE; i++){
-			fscanf(f,"%f",&(x[j*INPUT_SIZE + i]));
-		}
-		for(int i = 0 ; i < KERNEL_SIZE; i++){
-			fscanf(f,"%f",&(w[j*KERNEL_SIZE + i]));
-		}
-		for(int i = 0 ; i < OUTPUT_SIZE; i++){
-			fscanf(f,"%f",&(ref[j*OUTPUT_SIZE + i]));
-		}
-	}
+	/* connect to the leds */
+	PYNQ_loadBitstream((char * )"conv2d.bit");
+	PYNQ_MMIO_WINDOW led, hls;  // hls is AXILITE interface
+	PYNQ_createMMIOWindow(&led, 0x40010000,8); //in this functino, it uses mmap; the address can be got from vivado; 8 is length
+	PYNQ_createMMIOWindow(&hls, 0x40000000,64); //0x00 to 0x2c is 32+12=44, plus 4bytes, 48 is enough. We give 64. It will allocate the whole page anyway, so it doesn't really matter how big you set this, as long as you don't go over 4kB; This is the MMIOwindow for hls_slave, we can write to here.
+
+	/* Alocating shared memory */
+	PYNQ_SHARED_MEMORY sm_x, sm_w, sm_b, sm_z;
+	PYNQ_allocatedSharedMemory(&sm_x, IN_CHANNEL * IN_SIZE * IN_SIZE * sizeof(float), 0);
+	PYNQ_allocatedSharedMemory(&sm_w, OUT_CHANNEL * IN_CHANNEL * KERNEL_SIZE * KERNEL_SIZE * sizeof(float), 0);
+	PYNQ_allocatedSharedMemory(&sm_b, OUT_CHANNEL * sizeof(float), 0);
+	PYNQ_allocatedSharedMemory(&sm_z, OUT_CHANNEL * OUT_SIZE * OUT_SIZE * sizeof(float), 0);
+
+	/* write to the address */
+	// The structure of var"led" can be got from doc
+	uint32_t * b_led = (uint32_t *) led.buffer;
+	b_led[1] = 0; // the TRI register
+	b_led[0] = 3; // DATA register
+
+	/* Now can write the input data to the memory address */
+	// One cannot write to the pointer to virtual memory, so cast it to float * first
+	float * virt_x = (float *) sm_x.pointer;
+	float * virt_w = (float *) sm_w.pointer;
+	float * virt_z = (float *) sm_z.pointer;
+
+	/* Copy addr over axilite */
+	// These can be found in the HLS Summary
+	memcpy(hls.buffer + 0x10, &(sm_x.physical_address), sizeof(size_t));
+	memcpy(hls.buffer + 0x1c, &(sm_w.physical_address), sizeof(size_t));
+	memcpy(hls.buffer + 0x28, &(sm_b.physical_address), sizeof(size_t));
+	memcpy(hls.buffer + 0x34, &(sm_z.physical_address), sizeof(size_t));
+
+    //Tensor X,R,W,B;
+    float* X = new float[0];
+    uint32_t X_size[3] = {0, 0, 0};
+    float* R = new float[0];
+    uint32_t R_size[3] = {0, 0, 0};
+    float* W = new float[0];
+    uint32_t W_size[4] = {0, 0, 0, 0};
+    float* B = new float[0];
+    uint32_t B_size[3] = {0, 0, 0};
+    printf("------------------------------\n");
+    printf("Testing Convolutional Layer...\n");
+    while(1){
+    	if(!readConvRaw(&X,X_size, &R,R_size, &W,W_size, &B,B_size, f)) break;
+
+    	float* Z = new float[R_size[0]*R_size[1]*R_size[2]];
+    	//Z.randomize(-1,1);
+    	printf("Test X:[%dx%dx%d] W:[%dx%d] Output channels: %d!\n",X_size[0],X_size[1],X_size[2], W_size[2],W_size[3],R_size[0]);
+
+    		//convBasic(&X,W,&B,&Z);
+    	if(W_size[2] == 3 && W_size[3] == 3) {
+    		if(X_size[0] > IN_CHANNEL || X_size[1] > IN_SIZE || X_size[2] > IN_SIZE || R_size[0] > OUT_CHANNEL) {
+        		printf("Input/Output too big\n"); continue;
+    		}
+    		//Use FPGA for Conv2D_3x3:
+			//        in_sm, w_sm,	b_sm, in_w,      in_h,      in_c,      out_c,     out_sm
+
+    	    memcpy(virt_x, X, sizeof(float) * X_size[0]*X_size[1]*X_size[2]);
+    	    memcpy(virt_w, W, sizeof(float) * W_size[0]*W_size[1]*W_size[2]*W_size[3]);
+
+    	    /* Start HLS by setting bit */
+    	    uint32_t * hls_ctrl = (uint32_t *) hls.buffer;
+    	    *hls_ctrl = 0b1;
+    	    while((*hls_ctrl & 0b100)){}; // waiting for the IDLE(the 3rd bit) to 1, then we can print out the result
+         
+    	    memcpy(Z, virt_z, sizeof(float) * R_size[0]*R_size[1]*R_size[2]);
+
+
+
+
+
+			compareTensorsRaw(Z,R_size,R,R_size,1e-3);
+    	} else {
+			printf("Skipped because not 3x3 kernel\n");
+    	}
+    	delete [] Z;
+    }
+    delete [] X;
+    delete [] R;
+    delete [] W;
+    delete [] B;
+    fclose(f);
+
+	PYNQ_closeMMIOWindow( &led);
+	PYNQ_closeMMIOWindow( &hls);
+	return 0;
 }
--- final_project/testbench.cpp
+++ final_project/testbench.cpp
@@ -6,206 +6,12 @@
 #include "stdint.h"
 #include <stdio.h>
 #include <stdexcept>
+#include "common.h"
 
-// void Tensor::write(FILE *f)
-// {
-// 	uint64_t num_params = size[0]*size[1]*size[2];
-// 	fwrite(&(size[0]),sizeof(size[0]),1,f);
-// 	fwrite(&(size[1]),sizeof(size[1]),1,f);
-// 	fwrite(&(size[2]),sizeof(size[2]),1,f);
-// 	fwrite(data[0][0],sizeof(data[0][0][0]),num_params,f);
-// }
+int main(){
+    auto f = openTestFile();
+    if(!f) return 1;
 
-// Tensor * padTensor(Tensor * X , uint32_t pad)
-// {
-// 	int N = X->size[1] +  pad *2;
-// 	int K = X->size[2] +  pad *2;
-// 	Tensor * Xpad = new Tensor(X->size[0], N,K);
-// 	for(int z = 0; z < X->size[0]; z++){
-// 		float ** xpad = (*Xpad)[z];
-// 		float ** x = (*X)[z];
-// 		for(int i = 0; i < N ;i++){
-// 			for(int j = 0; j < K; j++){
-// 				if((i >= pad) && (i < (N-pad)) &&
-// 							(j >=  pad) && (j <  (K- pad)))
-// 					xpad[i][j] = x[i-pad][j-pad];
-// 			}
-// 		}
-// 	}
-// 	return Xpad;
-// }
-
-static float Fabs(float a)
-{
-	return (a > 0) ? a : -a;
-}
-
-// Compare two Tensor arrays of length N 
-int compareTensorsRaw(float* a, uint32_t a_size[3], float* ref, uint32_t ref_size[3], float limit){
-	if(a_size[0] != ref_size[0] || (a_size[1] != ref_size[1]) 
-			|| (a_size[2] != ref_size[2])){
-		throw std::runtime_error("Tensor dimensions don't match ! \n");
-	}
-	int ret = 0;
-	double abs_diff = 0;
-	for(int z = 0; z < a_size[0]; z++){
-	for(int y = 0; y < a_size[1]; y++){
-	for(int x = 0; x < a_size[2]; x++){
-		int index = z*(a_size[1]*a_size[2]) + y*(a_size[2]) + x;
-		float diff = Fabs( a[index] - ref[index] );
-		if(diff > limit) {
-			if(ret == 0) printf("Tensors differ at: [%d][%d][%d] by %f \n",z,y,x,diff);
-			ret++;
-		}
-		abs_diff += diff;
-	}}}
-	printf("Total avg diff: %lf\n",abs_diff/(1.f * a_size[0] * a_size[1] * a_size[2]));
-	if(ret == 0){
-		printf("Tensors are equal!\n");
-	}
-	return ret;
-}
-
-void convBasic(float* X, uint32_t X_size[3],
-               float* W, uint32_t W_size[4],
-               float* B, uint32_t B_size[3],
-               float* Z, uint32_t Z_size[3])
-{
-	int in_chan = X_size[0];
-	int out_chan = Z_size[0];
-	int wx = X_size[1];
-	int wy = X_size[2];
-	int weight_wx = W_size[2];
-	int weight_wy = W_size[3];
-	for (int oc = 0; oc < out_chan; oc++) {
-    	for (int x = 0; x < wx - weight_wx + 1; x++) {
-    		for (int y = 0; y < wy - weight_wy + 1; y++) {
-        		float sum = 0;
-           		for (int ic = 0; ic < in_chan; ic++) {
-        			for (int ix = 0; ix < weight_wx; ix++) {
-        				for (int iy = 0; iy < weight_wy; iy++) {
-							sum += X[ic*wx*wy+(x+ix)*wy+y+iy] *
-        					       W[oc*in_chan*weight_wx*weight_wy+ic*weight_wx*weight_wy+ix*weight_wy+iy];
-        				}
-        			}
-           		}
-    			Z[oc*Z_size[1]*Z_size[2]+x*Z_size[2]+y] = sum + B[oc];
-    		}
-    	}
-	}
-}
-
-// Iterates in oc->ic->x->y->ix->iy order instead of oc->x->y->ic->ix->iy
-void convBasic2(float* X, uint32_t X_size[3],
-               float* W, uint32_t W_size[4],
-               float* B, uint32_t B_size[3],
-               float* Z, uint32_t Z_size[3])
-{
-	int in_chan = X_size[0];
-	int out_chan = Z_size[0];
-	int wx = X_size[1];
-	int wy = X_size[2];
-	int weight_wx = W_size[2];
-	int weight_wy = W_size[3];
-	int out_wx = wx - weight_wx + 1;
-	int out_wy = wy - weight_wy + 1;
-	for (int oc = 0; oc < out_chan; oc++) {
-		for (int x = 0; x < out_wx; x++) {
-			for (int y = 0; y < out_wy; y++) {
-				Z[oc*Z_size[1]*Z_size[2]+x*Z_size[2]+y] = B[oc];
-			}
-		}
-		for (int ic = 0; ic < in_chan; ic++) {
-			float * sums = new float[out_wx * out_wy](); // The sums for one input channel, zero-init
-			for (int x = 0; x < out_wx; x++) {
-				for (int y = 0; y < out_wy; y++) {
-					float sum = 0; // Sum for one input channel for one kernel application/output pixel
-					for (int ix = 0; ix < weight_wx; ix++) {
-						for (int iy = 0; iy < weight_wy; iy++) {
-							sum += X[ic*wx*wy+(x+ix)*wy+y+iy] *
-							       W[oc*in_chan*weight_wx*weight_wy+ic*weight_wx*weight_wy+ix*weight_wy+iy];
-						}
-					}
-					sums[x*out_wy+y] += sum;
-				}
-			}
-			for (int x = 0; x < out_wx; x++) {
-				for (int y = 0; y < out_wy; y++) {
-					Z[oc*Z_size[1]*Z_size[2]+x*Z_size[2]+y] += sums[x*out_wy+y];
-				}
-			}
-		}
-	}
-}
-
-//Returns 1 if realloc was necessary
-void resizeTensor(float ** out, uint32_t size[], uint32_t new_size[], int dimensions)
-{
-    uint32_t prev = 1;
-    uint32_t next = 1;
-    for(int i = 0; i < dimensions; i++) {
-		prev *= size[i];
-		next *= new_size[i];
-		size[i] = new_size[i];
-    }
-	if(prev != next){
-		delete [] *out;
-		*out = new float[next];
-	}
-}
-
-//Returns 0 on failure
-int readTensorSize(FILE * f, uint32_t size[3])
-{
-	for(int i = 0; i < 3 ;i++){
-		if(1 != fread(&(size[i]),sizeof(size[0]),1,f))
-			return 0;
-	}
-	return 1;
-}
-
-//Returns 0 on failure
-int readTensorRaw(FILE * f, float * out, uint32_t size[3])
-{
-	uint32_t num_params = size[0] * size[1] * size[2];
-	if(num_params != fread(out,sizeof(float),num_params,f))
-		return 0;
-	return 1;
-}
-
-//Returns 0 on failure
-int readConvRaw(float ** X, uint32_t X_size[3], float ** Ref, uint32_t Ref_size[3], float ** W, uint32_t W_size[4], float ** B, uint32_t B_size[3], FILE * f)
-{
-    uint32_t new_X_size[3];
-    if(!readTensorSize(f, new_X_size)) return 0;
-    resizeTensor(X, X_size, new_X_size, 3); 
-    if(!readTensorRaw(f, *X, X_size)) return 0;
-
-    uint32_t new_Ref_size[3];
-    if(!readTensorSize(f, new_Ref_size)) return 0;
-    resizeTensor(Ref, Ref_size, new_Ref_size, 3); 
-	if(!readTensorRaw(f, *Ref, Ref_size)) return 0;
-
-    uint32_t new_W_size[4] = {Ref_size[0]};
-    if(!readTensorSize(f, &new_W_size[1])) return 0;
-    resizeTensor(W, W_size, new_W_size, 4);
-	for(int i = 0; i < Ref_size[0] ; i++){
-    	if(i != 0) {
-        	if(!readTensorSize(f, &new_W_size[1])) return 0;
-    	}
-		if(!readTensorRaw(f, &(*W)[i*W_size[1]*W_size[2]*W_size[3]], &W_size[1])) return 0;
-	}
-
-    uint32_t new_B_size[3];
-    if(!readTensorSize(f, new_B_size)) return 0;
-    resizeTensor(B, B_size, new_B_size, 3); 
-    if(!readTensorRaw(f, *B, B_size)) return 0;
-
-	return 1;
-}
-
-void testConv(FILE * f)
-{
 	//Tensor X,R,W,B;
 	float* X = new float[0];
 	uint32_t X_size[3] = {0, 0, 0};
@@ -227,13 +33,13 @@ void testConv(FILE * f)
 
 		//convBasic(&X,W,&B,&Z);
 		if(W_size[2] == 3 && W_size[3] == 3) {
-			if(X_size[0] > IN_CHANNEL) printf("Too many input channels\n");
-			if(X_size[1] > IN_SIZE || X_size[2] > IN_SIZE) printf("Input too big\n");
-			if(R_size[0] > OUT_CHANNEL) printf("Too many output channels\n");
+    		if(X_size[0] > IN_CHANNEL || X_size[1] > IN_SIZE || X_size[2] > IN_SIZE || R_size[0] > OUT_CHANNEL) {
+        		printf("Input/Output too big\n"); continue;
+    		}
 			//Use FPGA for Conv2D_3x3:
     		//        in_sm, w_sm,	b_sm, in_w,      in_h,      in_c,      out_c,     out_sm
         	EntryConv(X,     W,     B,    X_size[1], X_size[2], X_size[0], R_size[0], Z     );
-			//convBasic2(X,X_size, W,W_size, B,B_size, Z,R_size);
+			//convBasic3(X,X_size, W,W_size, B,B_size, Z,R_size);
 			//convBasic(X,X_size, W,W_size, B,B_size, Z,R_size);
     		compareTensorsRaw(Z,R_size,R,R_size,1e-3);
 		} else {
@@ -246,23 +52,7 @@ void testConv(FILE * f)
 	delete [] W;
 	delete [] B;
 	fclose(f);
-}
 
-int main(){
-	FILE * f = fopen("data/conf_test.dat", "rb");
-	// Try to use absolute path for Vitis HLS
-	if(!f) f = fopen("/home/clyybber/projects/uni/cnnhw/final/ACCN-Homework/final_project/data/conv_test.dat","rb");
-	if(!f) f = fopen("/home/hewei/TUM/ACCN/ACCN-Homework/final_project/data/conv_test.dat", "rb");
-	if(f) {
-		testConv(f);
-	} else {
-		printf("test.dat not found\n");
-	}
-
-	/*
-	for(int i = 0; i < ntests; i++){
-		EntryConv(x + (i*INPUT_SIZE),w + (i*KERNEL_SIZE), z + (i*OUTPUT_SIZE));
-	}*/
 	return 0;
 }
 
