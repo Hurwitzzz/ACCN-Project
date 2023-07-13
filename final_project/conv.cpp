5928745273ecd981f68e7421988a99e9a3b9cbce
Lorenz Krakau <ge93mok@mytum.de>
2023-07-13 23:39:09 +0200

    Use multidimensional arrays

--- final_project/conv.cpp
+++ final_project/conv.cpp
@@ -5,16 +5,16 @@
 
 #define IS (OS+KS-1)
 template<int KS, int IN_C, int OS, int out_c>
-void Conv2D(dt IN[IN_C*IS*IS],
-	dt W[out_c*IN_C*KS*KS],
+void Conv2D(dt IN[IN_C][IS][IS],
+	dt W[out_c][IN_C][KS][KS],
 	dt B[out_c],
-	dt OUT[out_c*OS*OS])
+	dt OUT[out_c][OS][OS])
 {
     //int IS = OS+KS-1;
 	/* Store in BRAM */
-	dt in[KS*KS];
+	dt in[KS][KS];
 #pragma HLS array_partition variable=in complete
-	dt w[IN_C*KS*KS];
+	dt w[IN_C][KS][KS];
 	dt b[out_c];
 
 	loadBias: for(int oc = 0; oc < out_c; oc++) {
@@ -27,10 +27,12 @@ void Conv2D(dt IN[IN_C*IS*IS],
 
 		// load w
 		int oc_W_idx = oc * IN_C * KS * KS;
-		loadKernel: for(int i = 0; i < IN_C * KS * KS; i++) {
-#pragma HLS loop_tripcount max=IN_C
-//#pragma HLS pipeline II=1
-			w[i] = W[oc_W_idx+i]; // w = W[oc];
+		loadKernel: for(int ic = 0; ic < IN_C; ic++) {
+#pragma HLS LOOP_FLATTEN
+			for(int p = 0; p < KS; p++) {
+			for(int q = 0; q < KS; q++) {
+				w[ic][p][q] = W[oc][ic][p][q];
+			}}
 		}
 
 		int oc_OUT_idx = oc * OS * OS;
@@ -45,10 +47,7 @@ void Conv2D(dt IN[IN_C*IS*IS],
 			}
 
 			forEachIN_C: for(int ic = 0; ic < IN_C; ic++) {
-#pragma HLS loop_tripcount max=IN_C
 //#pragma HLS pipeline
-				int ic_IN_idx = ic * IS * IS;
-				int ic_W_idx = ic * KS * KS;
 
 				// load all but one column for in
 				loadINAtStart1: for(int p = 0; p < KS; p++) {
@@ -57,8 +56,7 @@ void Conv2D(dt IN[IN_C*IS*IS],
 					int y_plus_p_IN_idx = (y+p) * IS;
 					loadInAtStart2: for(int q = 0; q < KS - 1; q++) {
 #pragma HLS unroll
-						in[p_idx+q] = IN[ic_IN_idx+y_plus_p_IN_idx+q];	  // in[p][q] = IN[ic][y + p][q];
-						//in[p*KS+q] = IN[ic * IS*IS + (y+p) * in_w +q];
+						in[p][q] = IN[ic][y+p][q];	  // in[p][q] = IN[ic][y + p][q];
 					}
 				}
 
@@ -75,19 +73,17 @@ void Conv2D(dt IN[IN_C*IS*IS],
 						int y_plus_p_IN_idx = (y+p) * IS;
 						int xmk_plus_KS_minus_1_mod_KS = xmk+KS-1;
 						if(xmk_plus_KS_minus_1_mod_KS >= KS) xmk_plus_KS_minus_1_mod_KS -= KS;
-						in[p_idx+ xmk_plus_KS_minus_1_mod_KS] = IN[ic_IN_idx+y_plus_p_IN_idx+x+KS-1];   // in[p][q] = (q == KS - 1) ? IN[ic][y + p][x + q] : in[p][q + 1];
+						in[p][xmk_plus_KS_minus_1_mod_KS] = IN[ic][y+p][x+KS-1];
 					}
 
 					// Conv calculation
 					conv1: for(int p = 0; p < KS; p++) {
 
-						int p_idx = p * KS;
-						int y_plus_p_IN_idx = (y+p) * IS;
 						conv2: for (int q = 0; q < KS; q++) {
 //#pragma HLS unroll
     						int xmk_plus_q_mod_KS = xmk+q;
     						if(xmk_plus_q_mod_KS >= KS) xmk_plus_q_mod_KS -= KS;
-							acc += in[p_idx+ xmk_plus_q_mod_KS] * w[ic_W_idx+p_idx+q]; // in[p][q] * w[p][q];
+							acc += in[p][xmk_plus_q_mod_KS] * w[ic][p][q]; // in[p][q] * w[p][q];
 						}
 					}
 
@@ -102,7 +98,7 @@ void Conv2D(dt IN[IN_C*IS*IS],
 			// add bias and send one row to OUT (in DRAM)
 			writeRowToOUT: for(int x = 0; x < OS; x++) {
 #pragma HLS pipeline II=1
-				OUT[oc_OUT_idx+y_idx+x] = acc_row[x] + b[oc]; // OUT[oc][y][x] = acc_row[x] + B[oc];
+				OUT[oc][y][x] = acc_row[x] + b[oc]; // OUT[oc][y][x] = acc_row[x] + B[oc];
 			}
 		} //for y
 	}
@@ -140,19 +136,39 @@ void EntryConv(dt IN[MAX_IN_CHANNEL*MAX_IN_SIZE*MAX_IN_SIZE],
 	// ConvLayer(384,256,12,12,3,0),
     switch(kernel_size) {
         case 1:
-            Conv2D<7, 3, 128, 96>(IN, W, B, OUT);
+            Conv2D<7, 3, 128, 96>(
+                (float (*)[134][134]) IN,
+                (float (*)[3][7][7]) W,
+                B,
+                (float (*)[128][128]) OUT);
         	break;
 		case 2:
-            Conv2D<5, 96, 64, 256>(IN, W, B, OUT);
+            Conv2D<5, 96, 64, 256>(
+                (float (*)[68][68]) IN,
+                (float (*)[96][5][5]) W,
+                B,
+                (float (*)[64][64]) OUT);
         	break;
         case 3:
-            Conv2D<3, 256, 32, 384>(IN, W, B, OUT);
+            Conv2D<3, 256, 32, 384>(
+                (float (*)[34][34]) IN,
+                (float (*)[256][3][3]) W,
+                B,
+                (float (*)[32][32]) OUT);
         	break;
 		case 4:
-            Conv2D<3, 384, 14, 384>(IN, W, B, OUT);
+            Conv2D<3, 384, 14, 384>(
+                (float (*)[16][16]) IN,
+                (float (*)[384][3][3]) W,
+                B,
+                (float (*)[14][14]) OUT);
             break;
 		case 5:
-            Conv2D<3, 384, 12, 256>(IN, W, B, OUT);
+            Conv2D<3, 384, 12, 256>(
+                (float (*)[14][14]) IN,
+                (float (*)[384][3][3]) W,
+                B,
+                (float (*)[12][12]) OUT);
         	break;
     }
 
